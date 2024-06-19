f59c1259d7516c51a52a5f37b6fa877f61336958
FabianCol <fabiancol13@gmail.com>
2024-06-19 15:58:32 -0500

    Add metodo de registrar un empleado metodo post

--- src/main/java/com/fabiancol/controlador/EmpleadoControlador.java
+++ src/main/java/com/fabiancol/controlador/EmpleadoControlador.java
@@ -10,6 +10,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -23,10 +25,16 @@ public class EmpleadoControlador {
     @Autowired
     private IEmpleadoServicio empleadoServicio;
     
-     @GetMapping("/empleados")
+    @GetMapping("/empleados")
     public List<Empleado> obtenerEmpleados(){
         var empleados = empleadoServicio.listarEmpleados();
         empleados.forEach((empleado -> logger.info(empleado.toString())));
         return empleados;
+    }
+    
+    @PostMapping("/empleados")
+    public Empleado agregarEmpleado(@RequestBody Empleado empleado){
+    logger.info("Emplado a agregar: "+empleado);
+    return empleadoServicio.guardarEmpleado(empleado);
     }
 }
