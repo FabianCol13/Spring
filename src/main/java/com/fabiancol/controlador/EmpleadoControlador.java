0846bf93a92dde7545a266fbdc399410e53e6984
FabianCol <fabiancol13@gmail.com>
2024-06-19 16:23:22 -0500

    Se integra el metodo buscar por id

--- src/main/java/com/fabiancol/controlador/EmpleadoControlador.java
+++ src/main/java/com/fabiancol/controlador/EmpleadoControlador.java
@@ -1,5 +1,7 @@
 package com.fabiancol.controlador;
- // @author FabianCol
+// @author FabianCol
+
+import com.fabiancol.excepcion.ExcepcionRecursoNoEncontrado;
 import com.fabiancol.modelo.Empleado;
 import com.fabiancol.servicio.IEmpleadoServicio;
 import java.util.List;
@@ -9,32 +11,42 @@ import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping("rh-sena")
 @CrossOrigin(value = "http://localhost:3000")
 public class EmpleadoControlador {
 
-     private static final Logger logger =
-            LoggerFactory.getLogger(EmpleadoControlador.class);
+    private static final Logger logger
+            = LoggerFactory.getLogger(EmpleadoControlador.class);
 
     @Autowired
     private IEmpleadoServicio empleadoServicio;
-    
+
     @GetMapping("/empleados")
-    public List<Empleado> obtenerEmpleados(){
+    public List<Empleado> obtenerEmpleados() {
         var empleados = empleadoServicio.listarEmpleados();
         empleados.forEach((empleado -> logger.info(empleado.toString())));
         return empleados;
     }
-    
+
     @PostMapping("/empleados")
-    public Empleado agregarEmpleado(@RequestBody Empleado empleado){
-    logger.info("Emplado a agregar: "+empleado);
-    return empleadoServicio.guardarEmpleado(empleado);
+    public Empleado agregarEmpleado(@RequestBody Empleado empleado) {
+        logger.info("Emplado a agregar: " + empleado);
+        return empleadoServicio.guardarEmpleado(empleado);
+    }
+
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleado>consultarEmpleadoId(@PathVariable Integer id) {
+        Empleado empleado = empleadoServicio.buscarEmpleadoPorId(id);
+        if (empleado == null) {
+            throw new ExcepcionRecursoNoEncontrado("No se encontro el Id del empleado" + id);
+        }
+        return ResponseEntity.ok(empleado);
     }
 }
