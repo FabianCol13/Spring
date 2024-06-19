bb86fc6ff758f75e18ff851d01212f986aba170c
FabianCol <fabiancol13@gmail.com>
2024-06-18 19:00:07 -0500

    Ejemplo spring

---
+++ src/main/java/com/fabiancol/controlador/EmpleadoControlador.java
@@ -0,0 +1,32 @@
+package com.fabiancol.controlador;
+ // @author FabianCol
+import com.fabiancol.modelo.Empleado;
+import com.fabiancol.servicio.IEmpleadoServicio;
+import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("rh-sena")
+@CrossOrigin(value = "http://locahost:3000")
+public class EmpleadoControlador {
+
+     private static final Logger logger =
+            LoggerFactory.getLogger(EmpleadoControlador.class);
+
+    @Autowired
+    private IEmpleadoServicio empleadoServicio;
+    
+     @GetMapping("/empleados")
+    public List<Empleado> obtenerEmpleados(){
+        var empleados = empleadoServicio.listarEmpleados();
+        empleados.forEach((empleado -> logger.info(empleado.toString())));
+        return empleados;
+    }
+}
---
+++ src/main/java/com/fabiancol/modelo/Empleado.java
@@ -0,0 +1,23 @@
+package com.fabiancol.modelo;
+ // @author FabianCol
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer idEmpleado;
+    String nombreEmpleado;
+    String departamentoEmpleado;
+    Double sueldoEmpleado;
+}
+
